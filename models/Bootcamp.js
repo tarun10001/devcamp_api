1368c55215c3109679a3fcf110c61a966cb9c0a1
tarun10001 <tarunkumawat1994@gmail.com>
2024-10-09 09:36:04 +0530

    added geocoder and custom error handling

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -11,7 +13,7 @@ const BootcampSchema = new mongoose.Schema({
   slug: String,
   description: {
     type: String,
-    required: [true, "Please add a name"],
+    required: [true, "Please add a description"],
     maxLength: [500, "Description can not be more than 500 characters"],
   },
   website: {
@@ -38,16 +40,14 @@ const BootcampSchema = new mongoose.Schema({
   },
   location: {
     // GeoJSON Point
-    // type: {
-    //   type: String,
-    //   enum: ["Point"],
-    //   required: true,
-    // },
-    // coordinates: {
-    //   type: [Number],
-    //   required: true,
-    //   index: "2dsphere",
-    // },
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
+    },
     formattedAddress: String,
     street: String,
     city: String,
@@ -98,6 +98,32 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  // console.log("Slugify ran", this.name);
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
-const colors = require('colors');
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
 
 //Load env variables
 dotenv.config({ path: "./config/config.env" });
@@ -26,11 +27,15 @@ if (process.env.NODE_ENV === "development") {
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
